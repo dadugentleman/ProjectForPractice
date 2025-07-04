80b1e0c739c851da3e0fc73d112551a5cad3696d
Sandu Guzun <sanduguzun762@gmail.com>
2025-07-04 14:38:13 +0300

    Initial commit

---
+++ src/main/java/com/example/ProjectToPractice/ProjectToPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectToPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToPracticeApplication.class, args);
+	}
+
+}
